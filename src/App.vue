4fe57d3107cdf74000f68a1e19039dd8b5a739c5
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-11-20 14:38:32 +0100

    Moved map to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <script setup lang="ts">
-    import { useTemplateRef } from 'vue';
-    import { useDragInteractor } from './map/dragInteractor';
-    import { usePositionRectTracker } from './map/positionRectTracker';
-    import GridRenderer from './map/GridRenderer.vue';
-    import { useZoomInteractor } from './map/zoomInteractor';
-
-    const target = useTemplateRef("target")
-    const tracker = usePositionRectTracker(target)
-    const { pan, zoom, toUnitCoords } = tracker
-    useDragInteractor(target, { drag: pan })
-    useZoomInteractor(target, { zoom: (diff, position) => {
-        zoom(diff / 1000, toUnitCoords(position))
-    }})
+    import Map from './map/Map.vue';
 </script>
 
 <template>
-    <div :class="$style.target" ref="target">
-        <GridRenderer :tracker="tracker" />
-    </div>
+    <Map />
 </template>
 
-<style lang="scss" module>
-    .target {
-        position: relative;
-        background-color: red;
-        width: 50%;
-        height: 300px;
-        overflow: hidden;
-    }
-
-    .test {
-        position: absolute;
-        width: 100px;
-        height: 100px;
-        background-color: black;
-    }
-</style>
+<style lang="scss" module></style>
---
+++ src/map/Map.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    import { useTemplateRef } from 'vue';
+    import { useDragInteractor } from './dragInteractor';
+    import { usePositionRectTracker } from './positionRectTracker';
+    import GridRenderer from './GridRenderer.vue';
+    import { useZoomInteractor } from './zoomInteractor';
+
+    const target = useTemplateRef("target")
+    const tracker = usePositionRectTracker(target)
+    const { pan, zoom, toUnitCoords } = tracker
+    useDragInteractor(target, { drag: pan })
+    useZoomInteractor(target, { zoom: (diff, position) => {
+        zoom(diff / 1000, toUnitCoords(position))
+    }})
+</script>
+
+<template>
+    <div :class="$style.target" ref="target">
+        <GridRenderer :tracker="tracker" />
+    </div>
+</template>
+
+<style lang="scss" module>
+    .target {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: red;
+        overflow: hidden;
+    }
+
+    .test {
+        position: absolute;
+        width: 100px;
+        height: 100px;
+        background-color: black;
+    }
+</style>
