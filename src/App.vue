154d46ebe577bf338308032262347fa0914ce7be
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-11-26 13:07:29 +0100

    Added pluto system example

--- src/App.vue
+++ src/App.vue
@@ -26,21 +26,72 @@
         }
     })
 
-    objects.value.push({
-        icon: './icons/earth.svg',
-        position: new Vector2(-4_670_000, 0),
-        size: 12_742_000,
-        mass: 5.972e24,
-        velocity: new Vector2(0, -12.5742),
-    })
+    const NORMALIZE_FACTOR = 1 / 6.6743e-20
 
+    // Pluto
     objects.value.push({
         icon: './icons/moon.svg',
-        position: new Vector2(380_784_000, 0),
-        size: 3_474_800,
-        mass: 7.34767309e22,
-        velocity: new Vector2(0, 1_022),
+        position: new Vector2(2.12703919e+06, -3.08210900e-12),
+        size: 2_376_600,
+        mass: 869.3 * NORMALIZE_FACTOR,
+        velocity: new Vector2(-2.83643439e-03,  2.42173886e+01),
     })
+    // Charon
+    objects.value.push({
+        icon: './icons/moon.svg',
+        position: new Vector2(-1.74691048e+07, -1.48438086e+03),
+        size: 1_212_000,
+        mass: 106.1 * NORMALIZE_FACTOR,
+        velocity: new Vector2(2.59407652e-02, -1.98897004e+02),
+    })
+    // Nix
+    objects.value.push({
+        icon: './icons/moon.svg',
+        position: new Vector2(-4.83354857e+07, -7.42254914e+06),
+        size: 50_000,
+        mass: 1.50e-3 * NORMALIZE_FACTOR,
+        velocity: new Vector2(2.06564133e+01, -1.39465804e+02),
+    })
+    // Hydra
+    objects.value.push({
+        icon: './icons/moon.svg',
+        position: new Vector2(32554655.74929921, 56051327.32152872),
+        size: 50_900,
+        mass: 2.01e-3 * NORMALIZE_FACTOR,
+        velocity: new Vector2(-106.01932065, 62.51678508),
+    })
+    // Kerberos
+    objects.value.push({
+        icon: './icons/moon.svg',
+        position: new Vector2(-51255243.15214943, -26846953.65410998),
+        size: 19_000,
+        mass: 1e-5 * NORMALIZE_FACTOR, // (very low mass)
+        velocity: new Vector2(60.15678687, -115.36690241),
+    })
+    // Styx
+    objects.value.push({
+        icon: './icons/moon.svg',
+        position: new Vector2(-4022208.59039472, 42121539.42213004),
+        size: 16_000,
+        mass: 1e-5 * NORMALIZE_FACTOR, // (very low mass)
+        velocity: new Vector2(-153.58637481, -15.71068336),
+    })
+
+    // objects.value.push({
+    //     icon: './icons/earth.svg',
+    //     position: new Vector2(-4_670_000, 0),
+    //     size: 12_742_000,
+    //     mass: 5.972e24,
+    //     velocity: new Vector2(0, -12.5742),
+    // })
+
+    // objects.value.push({
+    //     icon: './icons/moon.svg',
+    //     position: new Vector2(380_784_000, 0),
+    //     size: 3_474_800,
+    //     mass: 7.34767309e22,
+    //     velocity: new Vector2(0, 1_022),
+    // })
 
     const icons = computed(() => objects.value.map((object) => ({
         src: object.icon,
