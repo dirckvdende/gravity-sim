5a73dc757bbd1771f2f60250e002d86525d0f554
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-11-27 14:03:02 +0100

    Removed speed from options store

--- src/stores/options.ts
+++ src/stores/options.ts
@@ -6,7 +6,6 @@ import { ref } from "vue";
  * Store with global options for the simulator
  */
 export const useOptionsStore = defineStore("sim-options", () => {
-    const speed = ref(1)
     const showBarycenter = ref(false)
-    return { speed, showBarycenter }
+    return { showBarycenter }
 })
