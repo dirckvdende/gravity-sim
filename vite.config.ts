f39795ef64981c5abf7ff8e2581bb0fc484c7cdd
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-12-01 13:58:54 +0100

    Re-added devtools

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from "vite-plugin-vue-devtools"
 
 // https://vite.dev/config/
 export default defineConfig({
     base: "/gravity-sim/",
     plugins: [
         vue(),
+        vueDevTools()
     ],
     resolve: {
         alias: {
