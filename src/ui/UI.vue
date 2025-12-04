e45f755bb5f41c8dd2e9a12f5b4bed4a1783d48b
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-12-04 12:25:12 +0100

    Re-added load menu

--- src/ui/UI.vue
+++ src/ui/UI.vue
@@ -3,12 +3,18 @@
     import BottomSettings from './BottomSettings.vue';
     import DarkMode from './DarkMode.vue';
     import TimeDisplay from './TimeDisplay.vue';
+    import LoadMenu from './LoadMenu.vue';
+    import { storeToRefs } from 'pinia';
+    import { useMenuStore } from '@/oldstores/menu';
+
+    const { activeMenu } = storeToRefs(useMenuStore())
 </script>
 
 <template>
     <UIContainer>
         <BottomSettings />
         <TimeDisplay />
+        <LoadMenu :visible="activeMenu == 'load'" />
     </UIContainer>
     <DarkMode />
 </template>
