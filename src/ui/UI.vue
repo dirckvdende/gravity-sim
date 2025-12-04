c155e0f532d9e6990496c11d3949b178812af411
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-12-04 11:52:53 +0100

    Re-implemented dark mode

--- src/ui/UI.vue
+++ src/ui/UI.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
     import UIContainer from './UIContainer.vue';
     import BottomSettings from './BottomSettings.vue';
+    import DarkMode from './DarkMode.vue';
 </script>
 
 <template>
     <UIContainer>
         <BottomSettings />
     </UIContainer>
+    <DarkMode />
 </template>
