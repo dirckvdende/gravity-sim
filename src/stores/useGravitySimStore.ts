01853c117535db68be96481026ecca8e9f38501b
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-12-08 16:17:50 +0100

    #10 - Reverted gravity sim store to name "sim"

--- src/stores/useGravitySimStore.ts
+++ src/stores/useGravitySimStore.ts
@@ -7,7 +7,7 @@ import { useTimedGravitySim } from "@/sim/useTimedGravitySim"
 import { serializer } from "@/serializer"
 
 /** Store running the gravity sim and storing tracked objects */
-export const useGravitySimStore = defineStore("objects", () => {
+export const useGravitySimStore = defineStore("sim", () => {
     const objects = ref<StyledGravityObject[]>([])
     const { paused, speed } = storeToRefs(useSettingsStore())
     const sim = useTimedGravitySim(objects, { paused, speed })
