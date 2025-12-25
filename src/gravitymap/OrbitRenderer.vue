bdf18f4b19243768314889e65dbf8abe5065abe8
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-12-25 14:19:01 +0100

    #82 - Added better path renderer to orbit renderer

--- src/gravitymap/OrbitRenderer.vue
+++ src/gravitymap/OrbitRenderer.vue
@@ -4,17 +4,21 @@
     import { useOrbitHistoryStore } from '@/stores/useOrbitHistoryStore';
     import { useSettingsStore } from '@/stores/useSettingsStore';
     import { watch } from 'vue';
+    import DynamicPathRenderer from './DynamicPathRenderer.vue';
+    import { useGravitySimStore } from '@/stores/useGravitySimStore';
 
     const { showOrbits } = storeToRefs(useSettingsStore())
     const store = useOrbitHistoryStore()
     const { orbits } = storeToRefs(store)
+    const { objects } = storeToRefs(useGravitySimStore())
 
     watch(showOrbits, () => store.clearOrbits())
 </script>
 
 <template>
-    <PathRenderer
+    <DynamicPathRenderer
         v-if="showOrbits"
-        v-for="orbit in orbits"
-        :points="orbit.points.filter((value) => value != null)" />
+        v-for="{ id, position} in objects"
+        :key="id"
+        :point="position" />
 </template>
