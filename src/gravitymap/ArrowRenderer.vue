7dbada9a9e209f57d82457c7a32e47c8ad5408f1
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2025-12-26 20:55:41 +0100

    #13 - Added arrow handles to arrows

--- src/gravitymap/ArrowRenderer.vue
+++ src/gravitymap/ArrowRenderer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
     import { defaultState, mapStateKey } from '@/map/state';
-    import type Vector2 from '@/util/linalg/Vector2';
+    import Vector2 from '@/util/linalg/Vector2';
     import { computed, inject } from 'vue';
 
     const { start, end } = defineProps<{
@@ -10,20 +10,46 @@
         end: Vector2
     }>()
 
+    const HANDLE_ANGLE = Math.PI * 0.22
+    const HANDLE_LENGTH = 10
+
     const { toPixelCoords } = inject(mapStateKey, defaultState())
     const startCoords = computed(() => toPixelCoords(start))
     const endCoords = computed(() => toPixelCoords(end))
+    const angle = computed(() => {
+        const diff = endCoords.value.subtract(startCoords.value)
+        return (diff.isZero() ? 0 : diff.angle()) + Math.PI
+    })
+    const leftHandleCoords = computed(() => {
+        return endCoords.value.add(
+            new Vector2(HANDLE_LENGTH, 0).rotate(angle.value - HANDLE_ANGLE))
+    })
+    const rightHandleCoords = computed(() => {
+        return endCoords.value.add(
+            new Vector2(HANDLE_LENGTH, 0).rotate(angle.value + HANDLE_ANGLE))
+    })
 </script>
 
 <template>
     <svg :class="$style.svg">
-        <!-- TODO: Implement this properly with arrow and better styling -->
         <line
             :class="$style.line"
             :x1="startCoords.x"
             :y1="startCoords.y"
             :x2="endCoords.x"
             :y2="endCoords.y" />
+        <line
+            :class="$style.line"
+            :x1="endCoords.x"
+            :y1="endCoords.y"
+            :x2="leftHandleCoords.x"
+            :y2="leftHandleCoords.y" />
+        <line
+            :class="$style.line"
+            :x1="endCoords.x"
+            :y1="endCoords.y"
+            :x2="rightHandleCoords.x"
+            :y2="rightHandleCoords.y" />
     </svg>
 </template>
 
@@ -39,6 +65,7 @@
             stroke: red;
             fill: none;
             stroke-width: 2;
+            stroke-linecap: round;
         }
     }
 </style>
