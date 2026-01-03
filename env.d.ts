73b8783316440917c58769fea1f1223398f47212
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-01-03 20:29:09 +0100

    #103 - Added proper typing of environment variables

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+    strictImportMetaEnv: unknown,
+}
+
+interface ImportMetaEnv {
+    /** Whether a watermark "DEMO" should be shown */
+    readonly VITE_DEMO_WATERMARK?: "false" | "true",
+    /**
+     * Whether a warning should be displayed if the user refreshes or exits the
+     * page
+     */
+    readonly VITE_EXIT_WARNING?: "false" | "true",
+}
