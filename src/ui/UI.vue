c5536493e3a537ae8071ff4c4261d859bcc51792
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-01-07 18:15:08 +0100

    #107 - Moved dark mode setting to composable

--- src/ui/UI.vue
+++ src/ui/UI.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
     import UIContainer from './UIContainer.vue';
     import BottomSettings from './menus/BottomSettings.vue';
-    import DarkMode from './DarkMode.vue';
     import TimeDisplay from './TimeDisplay.vue';
     import LoadMenu from './menus/LoadMenu.vue';
     import ObjectDetailsMenu from './menus/ObjectDetailsMenu.vue';
     import DemoOverlay from './DemoOverlay.vue';
     import ObjectEditMenu from './menus/ObjectEditMenu.vue';
+    import { useDarkMode } from '@/composables/useDarkMode';
+
+    useDarkMode()
 </script>
 
 <template>
@@ -17,6 +19,5 @@
         <ObjectEditMenu />
         <BottomSettings />
     </UIContainer>
-    <DarkMode />
     <DemoOverlay />
 </template>
