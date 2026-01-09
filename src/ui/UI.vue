5a1d3d11f8948b8289d1ce1b2b6e87219582de13
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-01-09 08:51:51 +0100

    #107 - Combined UI and UIContainer components

--- src/ui/UI.vue
+++ src/ui/UI.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-    import UIContainer from './UIContainer.vue';
     import BottomSettings from './menus/BottomSettings.vue';
     import TimeDisplay from './TimeDisplay.vue';
     import LoadMenu from './menus/LoadMenu.vue';
@@ -12,12 +11,31 @@
 </script>
 
 <template>
-    <UIContainer>
-        <TimeDisplay />
-        <LoadMenu />
-        <ObjectDetailsMenu />
-        <ObjectEditMenu />
-        <BottomSettings />
-    </UIContainer>
+    <div :class="$style.container">
+        <div>
+            <TimeDisplay />
+            <LoadMenu />
+            <ObjectDetailsMenu />
+            <ObjectEditMenu />
+            <BottomSettings />
+        </div>
+    </div>
     <DemoOverlay />
 </template>
+
+<style lang="scss" module>
+    .container {
+        position: fixed;
+        top: calc(.6em + env(safe-area-inset-top, 0px));
+        left: calc(.6em + env(safe-area-inset-left, 0px));
+        right: calc(.6em + env(safe-area-inset-right, 0px));
+        bottom: calc(.6em + env(safe-area-inset-bottom, 0px));
+        pointer-events: none;
+
+        & > div {
+            width: 0;
+            height: 0;
+            pointer-events: all;
+        }
+    }
+</style>
