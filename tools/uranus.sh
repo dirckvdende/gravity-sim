8daccbc7a12540546ab945015b2594d72b41fb44
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-01-18 18:05:33 +0100

    #71 - Added moons of Uranus

---
+++ tools/uranus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd $(dirname $0)
+mkdir out
+
+python3 horizons-download.py 500@7 799 > out/uranus.txt
+for i in $(seq 701 717)
+do
+    python3 horizons-download.py 500@7 $i > out/$i.txt
+done
